bafaec99b072466fc235393db9bbc3912a81fd3e
= <=>
2018-09-17 17:35:48 +0200

    Fraction modified

--- src/test/java/FractionTest.java
+++ src/test/java/FractionTest.java
@@ -1,4 +1,18 @@
-public class FractionTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FractionTest {
     private Fraction fraction;
+
+    @BeforeEach
+    void before() {
+        this.fraction = new Fraction(1,2);
+    }
+
+    @Test
+    void testdecimal (){
+             assertEquals(0.5,this.fraction.decimal(),10e-5);
+    }
 }
 
